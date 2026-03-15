c69a10d2ba2042b9fe687910431c85892812044c
Chandan_Kr_Sharma <b.techchandancs@gmail.com>
2026-03-15 23:01:31 +0530

    feat: implement notes CRUD API with Express and MongoDB

--- Day5/PlayBack/src/app.js
+++ Day5/PlayBack/src/app.js
@@ -17,8 +17,6 @@ app.post('/api/notes', async (req, res) => {
     })
 })
 
-
-
 app.post('/api/note/new', async (req, res) => {
     const {title, content, price} = req.body
 
@@ -30,5 +28,64 @@ app.post('/api/note/new', async (req, res) => {
         message: "Notes created successfully"
     })
 })
+
+app.get('/api/notes', async (req, res) => {
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"Notes fetched successfully",
+        notes
+    })
+})
+
+app.delete('/api/notes/:id', async (req, res) => {
+
+    try {
+
+        const id = req.params.id
+
+        const note = await noteModel.findByIdAndDelete(id)
+
+        if (!note) {
+            return res.status(404).json({
+                message: "Note not found"
+            })
+        }
+
+        res.status(200).json({
+            message: "Note deleted successfully",
+            data: note
+        })
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        })
+
+    }
+
+})
+
+//Single update karna 
+app.patch('/api/notes/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        const {price} = req.body
+
+        const update = await noteModel.findByIdAndUpdate(id, {price})
+
+        res.status(200).json({
+            message: "Note is updated successfully"
+        })
+    } catch(err) {
+        res.status(500).json({
+            message: "Server error",
+            error: err.message
+        })
+    }
+})
+
 
 module.exports = app
